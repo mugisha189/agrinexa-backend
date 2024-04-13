ca0777802ad919861287ab1b5c558b0bb57c1952
yves mugisha <yvesmugisha84@gmail.com>
2024-04-13 10:27:33 +0200

    changed all the endpoints

--- app/api/fields/endpoints.py
+++ app/api/fields/endpoints.py
@@ -33,32 +33,90 @@ async def get_fields_for_logged_in_user(authorize:bool = Depends(AuthRole(roles=
         )
 
 
-@router.get("/user/:id")
-async def get_fields_for_user_by_id(id:str):
-    fields = list(db.fields.find())
-    return fields
+@router.get("/user/{user_id}", 
+            summary="Get fields by ID for the user",
+            description="Retrieves fields by ID of the user.",
+            responses={
+                200: {"description": "Fields retrieved successfully."},
+                400: {"description": "Invalid field ID provided."},
+                401: {"description": "Unauthorized - User is not authenticated."},
+                404: {"description": "Fields not found."},
+                500: {"description": "Internal Server Error."},
+            })
+async def get_fields_for_user_by_id(user_id: str, 
+                                     authorize:bool = Depends(AuthRole(roles="Admin"))):
+    try:
+        if not ObjectId.is_valid(user_id):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Invalid user ID provided."
+            )
+        fields = list(db.fields.find({"owner": ObjectId(user_id)}))
+        return fields
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred while retrieving fields."
+        )
 
-@router.get("/{field_id}")
-async def get_field(field_id: str):
-    field = db.fields.find_one({"_id": field_id})
-    if field:
-        return field
-    else:
-        raise HTTPException(status_code=404, detail="Field not found")
+@router.get("/{field_id}", 
+            summary="Get a field by ID",
+            description="Retrieves a field by its ID.",
+            responses={
+                200: {"description": "Field retrieved successfully.", "model": Field},
+                404: {"description": "Field not found."},
+                500: {"description": "Internal Server Error."},
+            })
+async def get_field(field_id: str,authorize:bool = Depends(AuthRole(roles=["Admin","User"]))):
+    try:
+        field = db.fields.find_one({"_id": field_id})
+        if field:
+            return field
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Field not found")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="An error occurred while retrieving the field.")
 
-@router.put("/{field_id}")
-async def update_field(field_id: str, updated_field: Field):
-    updated_field_data = updated_field.dict()
-    result = db.fields.update_one({"_id": field_id}, {"$set": updated_field_data})
-    if result.modified_count == 1:
-        return {"message": "Field updated successfully"}
-    else:
-        raise HTTPException(status_code=404, detail="Field not found")
 
-@router.delete("/{field_id}")
-async def delete_field(field_id: str):
-    result = db.fields.delete_one({"_id": field_id})
-    if result.deleted_count == 1:
-        return {"message": "Field deleted successfully"}
-    else:
-        raise HTTPException(status_code=404, detail="Field not found")
+@router.put("/{field_id}", 
+            summary="Update a field by ID",
+            description="Updates a field by its ID.",
+            responses={
+                200: {"description": "Field updated successfully."},
+                404: {"description": "Field not found."},
+                500: {"description": "Internal Server Error."},
+            })
+async def update_field(field_id: str, updated_field: Field,authorize:bool = Depends(AuthRole(roles="Admin"))):
+    try:
+        updated_field_data = updated_field.dict()
+        result = db.fields.update_one({"_id": field_id}, {"$set": updated_field_data})
+        if result.modified_count == 1:
+            return {"message": "Field updated successfully"}
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Field not found")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="An error occurred while updating the field.")
+
+
+@router.delete("/{field_id}", 
+               summary="Delete a field by ID",
+               description="Deletes a field by its ID.",
+               responses={
+                   200: {"description": "Field deleted successfully."},
+                   404: {"description": "Field not found."},
+                   500: {"description": "Internal Server Error."},
+               })
+async def delete_field(field_id: str,authorize:bool = Depends(AuthRole(roles="Admin"))):
+    try:
+        result = db.fields.delete_one({"_id": field_id})
+        if result.deleted_count == 1:
+            return {"message": "Field deleted successfully"}
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Field not found")
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="An error occurred while deleting the field.")
