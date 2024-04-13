3ef7f27bfb641c8bf792c8a78784b4589c759616
yves mugisha <yvesmugisha84@gmail.com>
2024-04-13 11:37:40 +0200

    added the head request

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ from app.db import db
 from app.api import auth,fields,plantings,harvest,nutrients,weather,pests
 app = FastAPI(    title="Agrinexa",
     description="This is the documentation for Agrinexa, a platform for tracking fields and nutrients in agriculture. Backend developed by Mugisha Yves",)
+@app.head("/")
+async def handle_head_request():
+    return "Hello Agrinexa"
 app.include_router(auth.router)
 app.include_router(fields.router)
 app.include_router(plantings.router)
